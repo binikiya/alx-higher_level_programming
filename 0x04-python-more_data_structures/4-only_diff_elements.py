ef69c6569873b8bce5a7cfc229627076f30066c6
binikiya <biniyamkiya3682@gmail.com>
2021-09-15 03:14:11 -0700

    Python More Data Structures

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,7 @@
 #!/urs/bin/python3
 def only_diff_elements(set_1, set_2):
-    return set_1 ^ set_2
+    """
+    Return the set of all elements
+    that have different values
+    """
+    return (set_1 ^ set_2)
