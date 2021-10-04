2825d86482b28fd17843eb8f7766f3d9d5f66e60
binikiya <biniyamkiya3682@gmail.com>
2021-10-04 03:25:51 -0700

    Updated Python Classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,9 +47,7 @@ class Square:
     @posision.setter
     def position(self, value):
         """ setter of position """
-        if type(value) is not tuple or len(value) != 2 or \
-           type(value[0]) is not int or value[0] < 0 or \
-           type(value[1]) is not int or value[1] < 0:
+        if type(value) is not tuple or len(value) != 2 or type(value[0]) is not int or value[0] < 0 or type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of two positive integers")
         else:
             self.__position = value
