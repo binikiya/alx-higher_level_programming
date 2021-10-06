5568ab1b7bf7b4f3c4d94c2a23b58acdf8ad2f36
binikiya <biniyamkiya3682@gmail.com>
2021-10-05 23:43:31 -0700

    Python Test Driven Development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,7 +7,7 @@
 def text_indentation(text):
     """splits a text into lines along side "?", ":", "." folowed by two lines"""
     if type(text) is not str:
-        raise TypeError("text must be string")
+        raise TypeError("text must be a string")
     flag = 0
     for a in text:
         if flag == 0:
