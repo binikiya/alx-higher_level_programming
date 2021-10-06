21b8293cd89ec3a4bf1c4fc54365b8102868903c
binikiya <biniyamkiya3682@gmail.com>
2021-10-05 22:53:51 -0700

    Python Test Driven Development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,9 +6,9 @@
 
 def add_integer(a, b):
     """the function that adds two integers or floats"""
-    if type(a) is not int or type(a) is not float:
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) is not int or type(b) is not float:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
