0803890d8b588a629b808db7d05a8d36e65c1591
binikiya <biniyamkiya3682@gmail.com>
2021-10-05 23:25:49 -0700

    Python Test Driven Development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,19 +2,22 @@
 """
 5-text_indentation.py
 """
+
+
 def text_indentation(text):
+    """a function for text indentation"""
     if type(text) is not str:
         raise TypeError("text must be string")
     flag = 0
-    for i in text:
+    for a in text:
         if flag == 0:
-            if i == ' ':
+            if a == ' ':
                 continue
             else:
                 flag = 1
         if flag == 1:
-            if i == '?' or i == '.' or i = ':':
-                print(i)
+            if a == '?' or i == '.' or a = ':':
+                print(a)
                 print()
                 flag = 0
             else:
