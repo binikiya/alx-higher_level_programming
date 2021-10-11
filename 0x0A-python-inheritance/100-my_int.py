398a92833872782c096e740279a55ec607984cb6
binikiya <binikiya123@gmail.com>
2021-10-11 06:38:54 -0700

    Python Inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+100-my_int.py
+"""
+
+
+class my_int(int):
+    """a class that inherits from int"""
+    def __new__(cls, *args, **kwargs):
+        """creates the new instance of the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
+    def __eq__(self, other):
+        """what was != is now =="""
+        return int(self) != other
+
+    def __ne__(self, other):
+        """what was == is now !="""
+        return int(self) == other
---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+101-add_attribute.py
+"""
+
+
+def add_attribute(abj, att, value):
+    """Adds a new attribute if it's possible"""
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(abj, att, value)
