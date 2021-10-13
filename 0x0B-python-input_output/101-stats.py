08d7d0ac48c0761b5e76d05d765c6dc31faf44e5
binikiya <binikiya123@gmail.com>
2021-10-12 22:36:44 -0700

    Python Input Output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,7 +6,7 @@ import sys
 
 file_size = 0
 status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
-          "403": 0, "404": 0, "405": 0, "500": 0}
+                "403": 0, "404": 0, "405": 0, "500": 0}
 i = 0
 try:
     for line in sys.stdin:
@@ -14,7 +14,7 @@ try:
         if len(tokens) >= 2:
             a = i
             if tokens[-2] in status_tally:
-                status_tally[tokens[-1]] += 1
+                status_tally[tokens[-2]] += 1
                 i += 1
             try:
                 file_size += int(tokens[-1])
