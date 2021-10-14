99f27a8b3e94dcede8c1eb652e72a390549c6042
binikiya <binikiya123@gmail.com>
2021-10-14 04:57:27 -0700

    Python Test Driven Development

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+101-lazy_maxrix_mul.py
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """multiplies 2 matrice by using the module NumPy"""
+    return numpy.matmul(m_a, m_b)
