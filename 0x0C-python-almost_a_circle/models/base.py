c098d4389764207c45bdf2561c2450361127c59e
binikiya <binikiya123@gmail.com>
2021-10-19 02:59:14 -0700

    Python Almost A Circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,7 +14,7 @@ class Base:
 
     def __init__(self, id=None):
         """Initialize the base class"""
-        if id is None:
+        if id == None:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
         else:
@@ -23,14 +23,14 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of a list of dictionaries"""
-        if list_dictionaries is None:
+        if list_dictionaries == None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
 
     @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string"""
-        if json_string is None or len(json_string) == 0:
+        if json_string == None or len(json_string) == 0:
             return []
         return json.loads(json_string)
 
@@ -39,7 +39,7 @@ class Base:
         """writes the JSON string representation of list_objs to a file"""
         filename = cls.__name__ + ".json"
         lo = []
-        if list_objs is not None:
+        if list_objs != None:
             for i in list_objs:
                 lo.append(cls.to_dictionary(i))
         with open(filename, 'w') as f:
@@ -48,9 +48,9 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
-        if cls.__name__ is "Rectangle":
+        if cls.__name__ == "Rectangle":
             dummy = cls(1, 1)
-        elif cls.__name__ is "Square":
+        elif cls.__name__ == "Square":
             dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
@@ -74,11 +74,11 @@ class Base:
         filename = cls.__name__ + ".csv"
         with open(filename, 'w', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
-            if cls.__name__ is "Rectangle":
+            if cls.__name__ == "Rectangle":
                 for obj in list_objs:
                     csv_writer.writerow([obj.id, obj.width, obj.height,
                                          obj.x, obj.y])
-            elif cls.__name__ is "Square":
+            elif cls.__name__ == "Square":
                 for obj in list_objs:
                     csv_writer.writerow([obj.id, obj.size, obj.x, obj.y])
 
