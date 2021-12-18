d002054e9bb4d15b462cc0890c0e9d0fa27febab
binikiya <biniyamkiya3682@gamil.com>
2021-12-18 02:33:00 -0800

    Python Object Relational Mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cur.execute(query)
+    row = cur.fetchall()
+    for r in row:
+        print(r)
+    cur.close()
+    conn.close()
