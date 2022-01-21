f1e2615b52e64156c0e4b431f3f6b09c264835d0
binikiya <biniyamkiya3682@gamil.com>
2022-01-21 03:29:53 -0800

    Python Network 2

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,11 +2,14 @@
 """
 takes your GitHub credentials and uses the GitHub API to display your id
 """
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
 
-if __name__ == '__main__':
-    import requests
-    from requests.auth import HTTPBasicAuth
-    from sys import argv
-    r = requests.get('https://api.github.com/users/{}'.format(argv[1]),
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(r.json().get('id'))
+
+if __name__ == "__main__":
+    name, psd = sys.argv[1], sys.argv[2]
+    hdr = {"Accept": "application/vnd.github.v3+json"}
+    url = "https://api.github.com/user"
+    r = requests.get(url,  auth=(name, psd), headers=hdr)
+    print(r.json()["id"])
