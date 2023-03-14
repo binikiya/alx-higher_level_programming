bc01c9f45fa9ab02dfe8384efee2debdbf05bd1b
binikiya <biniyamkiya3682@gmail.com>
2023-03-14 06:44:09 +0000

    Updated

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
+
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const args = process.argv.slice(2);
-  args.sort();
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
   console.log(args[args.length - 2]);
 }
