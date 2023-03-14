f93428b32384b2a030a24cdbded78013225dba8b
binikiya <biniyamkiya3682@gmail.com>
2023-03-14 13:33:14 +0000

    Updated 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,19 @@
 #!/usr/bin/node
+const SquareP = require('./5-square');
 
-const Square2 = require('./5-square.js');
-module.exports = class Square extends Square2 {
+class Square extends SquareP {
   charPrint (c) {
     if (c === undefined) {
-      this.print();
+      c = 'X';
     }
-    else {
-      for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
       }
+      console.log(s);
     }
   }
-};
+}
+
+module.exports = Square;
