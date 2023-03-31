76bb89b825c8ca11bd5262c8f04b2528488b6caa
Biniyam <biniyamkiya3682@gmail.com>
2023-03-31 13:52:59 +0300

    Updated

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,3 +7,13 @@ if __name__ == '__main__':
     r = requests.get('https://alx-intranet.hbtn.io/status')
     text = r.text
     print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(text), text))
+"""
+Python script that fetches an URL with requests package
+"""
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
